4b940f13449a0db589dc9ed760fa1952ad398523
Jon <jon_larrea@hotmail.com>
2021-09-29 22:51:11 +0200

    Fixing logging

--- scripts/setup_ric.sh
+++ scripts/setup_ric.sh
@@ -6,6 +6,7 @@ exec 2>&1
 
 echo "Starting RIC installation..."
 
+sudo -i
 cd ~
 
 # Step 1
@@ -17,21 +18,21 @@ git submodule update --init --recursive --remote
 cd tools/k8s/etc
 sed -i 's/2.12.3/2.17.0/g' infra.rc
 cd ../bin/
-sudo ./gen-cloud-init.sh
+./gen-cloud-init.sh
 
 # Step 3
 mv k8s* k8s_install.sh
 sed -i 's/https:\/\/storage.googleapis.com\/kubernetes-helm\/helm-v/https:\/\/get.helm.sh\/helm-v/g' k8s_install.sh
 sed -i 's/4.15.0-45-lowlatency/$(uname -r)/g' k8s_install.sh
-sudo ./k8s_install.sh
+./k8s_install.sh
 
 # Step 4
 cd ~
 sed -i 's/kong-docker-kubernetes-ingress-controller.bintray.io\/kong-ingress-controller/kong\/kubernetes-ingress-controller/g' ~/dep/ric-dep/helm/infrastructure/subcharts/kong/values.yaml
 cd dep/bin/
-sudo ./deploy-ric-platform -f ../RECIPE_EXAMPLE/PLATFORM/example_recipe.yaml
+./deploy-ric-platform -f ../RECIPE_EXAMPLE/PLATFORM/example_recipe.yaml
 # Fix Tiller
-KUBE_EDITOR="sed -i 's/gcr.io\/kubernetes-helm\/tiller:v2.12.3/ghcr.io\/helm\/tiller:v2.17.0/g'" sudo -E kubectl edit deploy deployment-tiller-ricxapp -n ricinfra
+KUBE_EDITOR="sed -i 's/gcr.io\/kubernetes-helm\/tiller:v2.12.3/ghcr.io\/helm\/tiller:v2.17.0/g'" kubectl edit deploy deployment-tiller-ricxapp -n ricinfra
 
 # Step 5
 cd ..
@@ -41,4 +42,4 @@ curl --location --request POST "http://$(hostname):32080/onboard/api/v1/onboard/
 # Step 6
 curl --location --request POST "http://$(hostname):32080/appmgr/ric/v1/xapps"  --header 'Content-Type: application/json'  --data-raw '{"xappName": "hwxapp"}'
 
-echo "RIC installation completed!"
+echo "RIC installation completed!c"
